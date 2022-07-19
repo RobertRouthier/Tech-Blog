505f5ff04fe8586d8daf30eb5a84cdbb2590e60f
Robert Routhier <robrouth92@gmail.com>
2022-07-19 12:30:10 -0700

    before lunch

--- Develop/public/js/signup.js
+++ Develop/public/js/signup.js
@@ -1,26 +1,27 @@
 async function signupFormHandler(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (username && password) {
-      const response = await fetch('/api/user', {
-        method: 'POST',
-        body: JSON.stringify({
-          username,
-          password,
-        }),
-        headers: { 'Content-Type': 'application/json' },
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && email && password) {
+      const response = await fetch('/api/users', {
+          method: 'post',
+          body: JSON.stringify({
+              username,
+              email,
+              password
+          }),
+          headers: {'Content-Type': 'application/json'}
       });
       if (response.ok) {
-        document.location.replace('/dashboard');
+          alert('Account created! Logging you in now.');
+          document.location.replace('/dashboard');
       } else {
-        alert(response.statusText);
+          alert(response.statusText)
       }
-    }
   }
-  
-  document
-    .querySelector('#signup-form')
-    .addEventListener('submit', signupFormHandler);
+}
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
